1ea75acb65f7acc3bdb492baf7abe121e176717c
doszhan <doszhankonysuly@gmail.com>
2024-10-10 19:27:58 +0500

    controller

---
+++ src/main/java/com/iqanat/demo/Controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.iqanat.demo.Controller;
+
+
+import com.iqanat.demo.DTO.EmployeeDTO;
+import com.iqanat.demo.Service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        EmployeeDTO savedEmployee = employeeService.createEmployee(employeeDTO);
+
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
