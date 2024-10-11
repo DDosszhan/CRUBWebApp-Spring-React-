679b9458be1d0549b43d664cd7cac9ad5c24a46d
doszhan <doszhankonysuly@gmail.com>
2024-10-11 05:06:18 +0500

    icon

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const HeaderComponent = () => {
   return (
     <div>
-        <header>
-            <nav className='navbar navbar-dark bg-dark'>
-                <a className='navbar-brand' href='https://www.javaguides.net'>Employee Management System</a>
-            </nav>
-        </header>
+      <header>
+        <nav className='navbar navbar-dark bg-dark'>
+          <span className='navbar-brand'>
+            <a href='https://github.com/DDosszhan' target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-book" style={{ fontSize: '1.5rem', marginRight: '10px', color: 'white' }}></i>
+            </a>
+            Employee Management System
+          </span>
+        </nav>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
